dca4029699159c361e011f82c22d4a95b68256cf
Aryan RAJ <131025781+aryan2882@users.noreply.github.com>
2025-07-07 16:12:51 +0530

    Update bookings.js
    booking file name to lowercase

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Booking = require('../models/Booking');
+const Booking = require('../models/booking');
 const router = express.Router();
 const mongoose = require('mongoose');
 
